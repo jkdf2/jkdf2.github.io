f150c2e5d8504b9a31b5452409a1f775417011b4
Taylor Kline <taylor.kline@streamingfreebies.com>
2014-06-17 09:09:31 -0700

    Added mapID as a variable.

---
+++ customleaflet.js
@@ -0,0 +1,27 @@
+/*
+ * Playing around with leaflet.
+ */
+
+//sets up the map at the lat/long location
+var map = L.map('map').setView([36.11, -115.17285], 13);
+
+//sets up attribution properties on the map
+var mapID = "taylorkline.iheaicei";
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/' + mapID + '/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+      }).addTo(map);
+
+var popup = L.popup();
+
+
+function onMapClick(e) {
+   popup
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(map);
+}
+
+map.on('click', onMapClick);
