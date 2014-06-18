b0961961fd5c626ae41009334da4f81bcffc2ff0
Taylor Kline <taylor.kline@streamingfreebies.com>
2014-06-18 08:37:55 -0700

    Updated maps with SSL.

--- customleaflet.js
+++ customleaflet.js
@@ -9,7 +9,7 @@ var map = L.map('map').setView([36.11, -115.17285], 12);
 
 /* sets up attribution properties on the map */
 var mapID = "taylorkline.iheaicei";
-L.tileLayer('http://{s}.tiles.mapbox.com/v3/' + mapID + '/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/' + mapID + '/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
       '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
       'Imagery © <a href="http://mapbox.com">Mapbox</a>',
