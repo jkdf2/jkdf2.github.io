2978d16b8173f7f0ee451ccbd7e03c03169514a9
Taylor Kline <taylor.kline@streamingfreebies.com>
2014-07-03 10:49:14 -0700

    Added shell for heatmap.

---
+++ maps/heatmapleaflet.js
@@ -0,0 +1,16 @@
+/*
+ * Playing around with leaflet
+ * by creating a heatmap.
+ */
+
+/* sets up the map at the lat/long location */
+var map = L.map('map').setView([37, -97], 4);
+
+/* sets up attribution properties on the map */
+var mapID = "taylorkline.iheaicei";
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/' + mapID + '/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+      }).addTo(map);
